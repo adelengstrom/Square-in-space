7b191dc55f7bfe2de5624acbfaa8786b6cb7ac8f
adelengstro <ae0014@edu.turku.fi>
2020-10-02 11:17:55 +0300

    Added files

---
+++ Square in space/Assets/Scripts/PlatformMovementUpDown.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovementUpDown : MonoBehaviour
+{
+    private Vector3 positionA;
+    
+    private Vector3 positionB;
+
+    private Vector3 nextPosition;
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private Transform childTransform;
+
+    [SerializeField]
+    private Transform transformB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        positionA = childTransform.localPosition;
+        positionB = transformB.localPosition;
+        nextPosition = positionB;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        childTransform.localPosition = Vector3.MoveTowards(childTransform.localPosition, nextPosition, speed * Time.deltaTime);
+
+        if(Vector3.Distance(childTransform.localPosition, nextPosition) <= 0.1)
+        {
+            ChangeDestination();
+        }
+    }
+
+    private void ChangeDestination()
+    {
+        nextPosition = nextPosition != positionA ? positionA : positionB;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name.Equals("Player"))
+        {
+            other.transform.SetParent(childTransform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        other.transform.SetParent(null);
+    }
+}
---
+++ Square in space/Assets/Scripts/Platformer.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platformer : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed;
+    public float jumpForce;
+    bool isGrounded = false;
+    public Transform isGroundedChecker;
+    public float checkGroundRadius;
+    public LayerMask groundLayer;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    public float rememberGroundedFor;
+    float lastTimeGrounded;
+    public int defaultAdditionalJumps = 1;
+    int additionalJumps;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+        CheckIfGrounded();
+        BetterJump();
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float moveBy = x * speed;
+        rb.velocity = new Vector2(moveBy, rb.velocity.y);
+
+
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && (isGrounded || Time.time - lastTimeGrounded <=
+            rememberGroundedFor || additionalJumps > 0))
+        {
+            SoundManager.PlaySound("Jump");
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            additionalJumps--;
+        }
+    }
+
+    void CheckIfGrounded()
+    {
+        Collider2D collider = Physics2D.OverlapCircle(isGroundedChecker.position,
+            checkGroundRadius, groundLayer);
+
+        if (collider != null)
+        {
+            isGrounded = true;
+            additionalJumps = defaultAdditionalJumps;
+        }
+        else
+        {
+            if (isGrounded)
+            {
+                lastTimeGrounded = Time.time;
+            }
+            isGrounded = false;
+        }
+    }
+
+    void BetterJump()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity * (fallMultiplier - 1) *
+     Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity * (lowJumpMultiplier - 1) *
+    Time.deltaTime;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name.Equals("Moving Platform"))
+            this.transform.parent = col.transform;
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.name.Equals("Moving Platform"))
+            this.transform.parent = null;
+    }
+
+
+    //Picking the pickup
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+       if(other.gameObject.CompareTag("Pickup"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+}
